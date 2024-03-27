8f477f09f572c5c0f226c97672a8910c47be5043
imsushmoy <hiraku.kazuma5@gmail.com>
2024-03-27 10:23:59 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+  </header>
+
+  <main>
+
+Features
+Pricing
+Resources
+
+Login
+Sign Up
+
+More than just shorter links
+
+Build your brand’s recognition and get detailed insights 
+on how your links are performing.
+
+Get Started
+
+Shorten a link here...
+
+Shorten It!
+
+Advanced Statistics
+
+Track how your links are performing across the web with our 
+advanced statistics dashboard.
+
+Brand Recognition
+
+Boost your brand recognition with each click. Generic links don’t 
+mean a thing. Branded links help instil confidence in your content.
+
+Detailed Records
+
+Gain insights into who is clicking your links. Knowing when and where 
+people engage with your content helps inform better decisions.
+
+Fully Customizable
+
+Improve brand awareness and content discoverability through customizable 
+links, supercharging audience engagement.
+
+Boost your links today
+
+Get Started
+
+Features
+
+Link Shortening
+Branded Links
+Analytics
+
+Resources
+
+Blog
+Developers
+Support
+
+Company
+
+About
+Our Team
+Careers
+Contact
+
+<div class="attribution">
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+  Coded by <a href="#">Your Name Here</a>.
+</div>
+</main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
