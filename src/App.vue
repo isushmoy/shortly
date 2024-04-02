5fa1c8f233ee0b263fc2b8bee773d92fa912c2a0
imsushmoy <hiraku.kazuma5@gmail.com>
2024-04-02 17:24:29 +0600

    Fix the UI for desktop interface

--- src/App.vue
+++ src/App.vue
@@ -56,58 +56,84 @@ async function clearItems() {
   <header>
     <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
   </header>
+  <navbar />
 
   <main class="" data-bs-theme="auto">
-    <div class="top container p-3" style="margin-bottom: 5rem;">
-      <navbar />
+    <div
+      class="top container p-3 d-lg-flex flex-row-reverse"
+      style="margin-bottom: 5rem"
+    >
       <img
-        class="img-fluid mt-2"
+        class="img-fluid mt-2 px-5 col-lg-7"
         src="./assets/images/illustration-working.svg"
         alt=""
+        style="position: sticky; left: 0"
       />
-      <h1 class="fw-bolder text-center mt-5">More than just shorter links</h1>
-      <p class="text-center">
-        Build your brand’s recognition and get detailed insights on how your
-        links are performing.
-      </p>
-      <div class="middle-button d-flex justify-content-center">
-        <button class="btn cyan-button rounded-pill px-4 mb-5">
-          Get Started
-        </button>
+      <div class="head-text mx-auto px-1" style="padding-right: 5rem">
+        <h1
+          class="fs-lg-1 fw-bolder text-center text-lg-start mt-5"
+          style="font-size: 2.5rem"
+        >
+          More than just shorter links
+        </h1>
+        <p class="text-center text-lg-start">
+          Build your brand’s recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <div
+          class="centered-button d-flex justify-content-center justify-content-lg-start"
+        >
+          <button class="btn cyan-button rounded-pill px-4 mb-5 mt-lg-3">
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
     <div class="middle p-3">
-      <div class="bg-shorten card mb-3" style="background-color: #3a3053; margin-top: -5.8rem;">
-        <img
-          src="./assets/images/bg-shorten-mobile.svg"
-          class="card-img d-block d-xl-none"
-          alt="bg-shorten-mobile"
-          style="max-height: 9.5rem"
-        />
-        <img
-          src="./assets/images/bg-shorten-desktop.svg"
-          class="card-img d-none d-xl-block"
-          alt="bg-shorten-desktop"
-          style="max-height: 10rem"
-        />
-        <div class="card-img-overlay p-4">
-          <form action="" class="row px-1 px-sm-2">
-            <input
-              class="form-control mt-xl-1"
-              :class="{ 'is-invalid': isInvalid }"
-              v-model="longUrl"
-              type="text"
-              placeholder="Shorten a link here..."
-              required
-            />
-            <div class="invalid-feedback">Please add a link</div>
-            <button class="btn cyan-button mt-4" @click.prevent="shortenUrl()">
-              Shorten It!
-            </button>
-          </form>
+      <div class="container">
+        <div
+          class="bg-shorten card mb-3"
+          style="background-color: #3a3053; margin-top: -5.8rem"
+        >
+          <img
+            src="./assets/images/bg-shorten-mobile.svg"
+            class="card-img d-block d-lg-none"
+            alt="bg-shorten-mobile"
+            style="max-height: 9.3rem"
+          />
+          <img
+            src="./assets/images/bg-shorten-desktop.svg"
+            class="card-img d-none d-lg-block"
+            alt="bg-shorten-desktop"
+            style="max-height: 6.7rem"
+          />
+          <div class="card-img-overlay p-4">
+            <form action="" class="row px-1 mt-lg-2">
+              <div class="col-lg-10">
+                <input
+                  class="form-control"
+                  :class="{ 'is-invalid': isInvalid }"
+                  v-model="longUrl"
+                  type="text"
+                  placeholder="Shorten a link here..."
+                  required
+                />
+                <div class="invalid-feedback">Please add a link</div>
+              </div>
+              <div class="col-lg-2 row mx-auto">
+                <button
+                  class="btn cyan-button mt-3 mt-lg-0 mb-2"
+                  @click.prevent="shortenUrl()"
+                  style="height: 2.4rem;"
+                >
+                  Shorten It!
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-      <div class="d-flex justify-content-end">
+      <div class="d-flex justify-content-end container">
         <button
           class="btn cyan-color mb-3"
           v-if="savedUrls.length !== 0"
@@ -121,17 +147,18 @@ async function clearItems() {
         v-for="(url, index) in savedUrls"
         :key="index"
         style="list-style-type: none"
+        class="container"
       >
         <div class="result mb-3">
           <div class="card">
             <div class="card-header">{{ url.longUrl }}</div>
-            <div class="card-body">
+            <div class="card-body d-lg-flex flex-row justify-content-between">
               <a class="card-text cyan-" :href="url.shortUrl" target="_blank">
                 {{ url.shortUrl }}
               </a>
               <button
-                class="btn cyan-button col-12 mt-3"
-                @click="copyUrl(url.shortUrl)" 
+                class="btn cyan-button col-auto mt-3 mt-lg-0"
+                @click="copyUrl(url.shortUrl)"
               >
                 Copy
               </button>
@@ -139,52 +166,53 @@ async function clearItems() {
           </div>
         </div>
       </li>
-      <div class="statistics mt-5 mb-5 d-flex flex-column align-items-center">
-        <h3>Advanced Statistics</h3>
+      <div class="statistics mt-5 mb-5 mb-lg-0 d-flex flex-column align-items-center" style="padding-top: 2.5rem;">
+        <h3 class="fw-bold">Advanced Statistics</h3>
         <p class="text-center" style="max-width: 30rem">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
       </div>
-      <div class="statistics d-flex flex-column align-items-center">
+      <div
+        class="statistics container d-flex flex-column align-items-center flex-lg-row justify-content-lg-evenly"
+      >
         <div
-          class="brand-recognition card text-center d-flex align-items-center"
-          style="width: 18rem"
+          class="brand-recognition card text-center text-lg-start d-flex align-items-center align-items-lg-start"
         >
-          <div class="circle" style="margin-top: -2rem">
+          <div class="circle ms-lg-3" style="margin-top: -2rem">
             <img src="./assets/images/icon-brand-recognition.svg" alt="" />
           </div>
           <div class="card-body mb-2">
             <h5 class="card-title">Brand Recognition</h5>
             <p class="card-text">
               Boost your brand recognition with each click. Generic links don’t
-              mean a thing. Branded links help instil confidence in your content.
+              mean a thing. Branded links help instil confidence in your
+              content.
             </p>
           </div>
         </div>
         <div class="vertical-line"></div>
         <div
-          class="detailed-records card text-center d-flex align-items-center"
-          style="width: 18rem"
+          class="detailed-records card text-center text-lg-start d-flex align-items-center align-items-lg-start"
         >
-          <div class="circle" style="margin-top: -2rem">
+          <div class="circle ms-lg-3" style="margin-top: -2rem">
             <img src="./assets/images/icon-detailed-records.svg" alt="" />
           </div>
           <div class="card-body mb-2">
             <h5 class="card-title">Detailed Records</h5>
             <p class="card-text">
               Gain insights into who is clicking your links. Knowing when and
-              where people engage with your content helps inform better decisions.
+              where people engage with your content helps inform better
+              decisions.
             </p>
           </div>
         </div>
         <div class="vertical-line"></div>
         <div
-          class="fully-customizable mb-5 card text-center d-flex align-items-center"
-          style="width: 18rem"
+          class="fully-customizable mb-5 mb-lg-0 card text-center text-lg-start d-flex align-items-center align-items-lg-start"
         >
-          <div class="circle" style="margin-top: -2rem">
-            <img src="./assets/images/icon-fully-customizable.svg" alt="" />
+          <div class="circle ms-lg-3" style="margin-top: -2rem">
+            <img src="./assets/images/icon-fully-customizable.svg" alt=""/>
           </div>
           <div class="card-body mb-2">
             <h5 class="card-title">Fully Customizable</h5>
@@ -197,27 +225,30 @@ async function clearItems() {
       </div>
     </div>
   </main>
-    <div class="boost d-flex flex-column align-items-center justify-content-center">
-      <h2 class="mt-5">Boost your links today</h2>
-      <button class="btn cyan-button rounded-pill px-4 mb-5">Get Started</button>
-    </div>
-    <div class="bottom text-center">
-      <h2 class="pt-5 pb-4 fw-bold">Shortly</h2>
-      <div class="features mb-4">
+  <div
+    class="boost d-flex flex-column align-items-center justify-content-center"
+  >
+    <h2 class="mt-5">Boost your links today</h2>
+    <button class="btn cyan-button rounded-pill px-4 mb-5">Get Started</button>
+  </div>
+  <div class="bottom">
+    <div class="container text-center text-lg-start d-lg-flex justify-content-lg-between p-lg-5">
+      <h2 class="pt-5 pt-lg-3 pe-lg-5 pb-4 fw-bold">Shortly</h2>
+      <div class="features mb-4 p-lg-3">
         <h4 href="">Features</h4>
   
         <a href=""> Link Shortening </a>
         <a href=""> Branded Links </a>
         <a href=""> Analytics </a>
       </div>
-      <div class="resources mb-4">
+      <div class="resources mb-4 pt-lg-3 pe-lg-3">
         <h4>Resources</h4>
   
         <a href=""> Blog </a>
         <a href=""> Developers </a>
         <a href=""> Support </a>
       </div>
-      <div class="company mb-5">
+      <div class="company mb-5 pt-lg-3 pe-lg-3">
         <h4>Company</h4>
   
         <a href=""> About </a>
@@ -225,7 +256,7 @@ async function clearItems() {
         <a href=""> Careers </a>
         <a href=""> Contact </a>
       </div>
-      <div class="social-media">
+      <div class="social-media pt-lg-3">
         <a href="#">
           <i class="fa-brands fa-facebook fa-xl"></i>
         </a>
@@ -240,10 +271,58 @@ async function clearItems() {
         </a>
       </div>
     </div>
-    <footer class="attribution text-center text-secondary p-4" style="background-color: hsl(260, 8%, 14%)">Coded by <a href="#">Sushmoy</a>.</footer>
+  </div>
+  <footer
+    class="attribution text-center text-secondary p-4"
+    style="background-color: hsl(260, 8%, 14%)"
+  >
+    Coded by <a href="#">Sushmoy</a>.
+  </footer>
 </template>
 
 <style scoped>
+@media (min-width: 992px) {
+  .bg-shorten {
+    margin-top: -3.8rem !important;
+  }
+  .vertical-line {
+    border: 0.3rem hsl(180, 66%, 49%) solid !important;
+    transform: rotate(90deg);
+    height: 37rem !important;
+  }
+  .statistics{
+    margin-top: -5rem;
+  }
+  .statistics .card{
+    width: 20rem !important;
+    height: 14.5rem;
+    z-index: 1000;
+  }
+
+  .brand-recognition{
+    margin-top: -3rem;
+  }
+  .detailed-records{
+    margin-top: 2rem;
+  }
+  .fully-customizable{
+    margin-top: 5rem;
+  }
+  /* .bottom{
+    display: flex;
+    flex-direction: row;
+    align-items: start;
+    padding: 2rem;
+    
+  } */
+
+}
+
+.statistics .card{
+    width: 19rem !important;
+    height: 14.5rem;
+    z-index: 1000;
+  }
 
 /* copied function
 .copied{
@@ -252,12 +331,12 @@ async function clearItems() {
 
 :style="{ backgroundColor: copiedIndex === index ? 'hsl(257, 27%, 26%)' : 'hsl(180, 66%, 49%)' }" */
 
-h1{
+/* h1{
   font-size: 2rem;
-}
+} */
 
 /* statistics */
-.statistics .card-title{
+.statistics .card-title {
   margin-top: -1rem;
   font-weight: 700;
   margin-bottom: 0.7rem;
@@ -268,16 +347,16 @@ h1{
 }
 
 .statistics .card {
-  padding: 0 0.5rem;
+  padding: 0 1rem;
 }
 
-.middle{
+.middle {
   background-color: #f0f1f6;
   /* padding: 4rem; */
 }
 
 /* social media */
-.social-media a{
+.social-media a {
   width: max-content;
   display: inline-block !important;
 }
@@ -289,44 +368,45 @@ h1{
   background-color: #3a3053;
   color: white;
 }
-.vertical-line{
+.vertical-line {
   border: 0.3rem hsl(180, 66%, 49%) solid;
   height: 5rem;
 }
 
 /* bottom */
-.bottom, .bottom h4{
+.bottom,
+.bottom h4 {
   background-color: hsl(260, 8%, 14%);
   color: #fff;
   font-size: 1rem;
 }
-.bottom a{
+.bottom a {
   color: hsl(257, 7%, 63%);
   display: block;
 }
-.bottom i{
+.bottom i {
   margin: 0.7rem;
 }
 
 /* colors */
 
-p{
+p {
   color: hsl(257, 7%, 63%);
 }
-.grayish-violet{
+.grayish-violet {
   background-color: hsl(255, 11%, 22%);
 }
-.cyan-color{
+.cyan-color {
   color: hsl(180, 66%, 49%);
 }
-.cyan-color:hover{
+.cyan-color:hover {
   color: hsl(180, 66%, 39%);
 }
 .cyan-button {
   background-color: hsl(180, 66%, 49%);
   color: white;
 }
-.cyan-button:hover{
+.cyan-button:hover {
   background-color: hsl(180, 66%, 39%);
 }
 .cyan-button:active {
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,56 +1,71 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <a href="#">
-          <img class="navbar-brand" src="../assets/images/logo.svg" alt="logo">
-        </a>
-        <!-- <a class="navbar-brand" href="#">Shortly</a> -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse hide mt-2" id="navbarSupportedContent" style="">
-          <ul class="navbar-nav bg-body-secondary rounded text-center p-4 me-auto mb-2 mb-lg-0 ">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Resources</a>
-            </li>
-            <hr style="color: white;">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Login</a>
-            </li>
-            <li class="nav-item mx-auto">
-              <button class="nav-link active cyan-button btn rounded-pill px-5" aria-current="page" href="#">Sign Up</button>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a href="#" class="col-auto navbar-brand ms-2">
+        <img class="" src="../assets/images/logo.svg" alt="logo" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation" style="border: none;"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse hide navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item ">
+            <a class="nav-link active" aria-current="page" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"
+              >Resources</a
+            >
+          </li>
+          <hr style="color: white" />
+        </ul>
+        <ul class="navbar-nav bottom-ul me-2 mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Login</a>
+          </li>
+          <li class="nav-item mx-auto">
+            <button
+              class="nav-link active cyan-button btn rounded-pill px-md-4 py-md-2 px-5 fw-bold"
+              aria-current="page"
+              href="#"
+              style="color: #fff"
+            >
+              Sign Up
+            </button>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </template>
+    </div>
+  </nav>
+</template>
 
 <style scoped>
-.navbar-brand{
+.navbar-brand {
   font-size: 2rem;
   font-weight: 700;
 }
-.navbar-nav {
-  background-color: hsl(257, 27%, 26%) !important;
+
+.nav-link:not(button) {
+  margin-right: 0.5rem;
+  color: gray !important;
+  font-weight: 600;
 }
-.nav-link{
-  margin: 0.5rem;
-  color: white !important;
-  font-size: 1rem;
-  font-weight: 700;
-}
-.cyan-button{
+.cyan-button {
   background-color: hsl(180, 66%, 49%);
+  /* padding: 0.4rem; */
 }
 /* .cyan-button:active {
   background-color: hsl(257, 27%, 26%);
@@ -58,5 +73,23 @@
 .cyan-button:hover {
   background-color: hsl(180, 66%, 39%);
 }
-  
+
+@media (max-width: 62rem){
+  .navbar-collapse {
+  background-color: hsl(257, 27%, 26%) !important;
+  text-align: center;
+  border-radius: 0.5rem;
+  padding: 2rem;
+  margin: 1rem;
+  }
+.navbar .nav-link {
+  color: #fff !important;
+}
+.bottom-ul .nav-link:not(button){
+  margin-bottom: 0.5rem;
+}
+/* .bottom-ul .nav-item button{
+  padding-right: 10rem;
+} */
+}
 </style>
